aea2a53cd9bcbf6cf60916d6ba18896d09833b94
vionaosiako <viona.osiako@student.moringaschool.com>
2024-10-23 12:59:53 +0300

    Add project files.

---
+++ ControlStructure/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your age:");
+int age = int.Parse(Console.ReadLine());
+if (age >= 18)
+{
+    Console.WriteLine("You are eligible to vote.");
+}
+
